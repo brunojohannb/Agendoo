e92b66bb61e74b3d90e0587e4586d9a7febd3118
Bruno Johann B. <brunojohannber@gmail.com>
2024-11-14 18:23:10 -0300

    Tela de Login

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Login from './Components/Login/Login'
 function App() {
 
   return (
-    <div className="app">
+    <div className="App">
       <Login/>
     </div>
   )
--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,16 +1,39 @@
-import React from 'react'
+import { FaUser, FaLock } from "react-icons/fa"
+import { useState } from "react"
+import "./Login.css"
 
 const Login = () => {
 
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log (username, password);
+  }
+
   return (
     <div className='container'>
-        <form>
-          <h1>Login</h1>
-          <div>
-            <input type="email" placeholder='E-mail'/>
+        <form onSubmit={handleSubmit}>
+          <h1 className="logo">RICHMO</h1>
+          <div className="inputs">
+            <input
+            type="email" 
+            placeholder='E-mail' 
+            onChange={ (e) => setUsername(e.target.value) }/>
+            <FaUser className="icon"/>
           </div>
-          <div>
-            <input type="password" placeholder='Senha'/>
+          <div className="inputs">
+            <input
+            type="password" 
+            placeholder='Senha'
+            onChange={ (e) => setPassword(e.target.value) }/>
+            <FaLock className="icon"/>
+          </div>
+          <button>ENTRAR</button>
+          <div className="resetPassword">
+            <a href="#">Esqueceu a senha?</a>
           </div>
         </form>
      </div>
